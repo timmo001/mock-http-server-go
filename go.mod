63751547d07dbb6ad2c4c07916681c6e5193b4a2
Aidan Timson <aidan@timmo.dev>
2024-08-27 14:54:42 +0100

    Create echo server

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/timmo001/mock-http-server-go
+
+go 1.23.0
+
+require github.com/joho/godotenv v1.5.1
---
+++ server.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// Setup a logger to a file
+	f, err := os.OpenFile("mockhttpserver.log", os.O_RDWR|os.O_CREATE|os.O_APPEND|os.O_TRUNC, 0666)
+	CheckErr(err)
+
+	defer f.Close()
+	log.SetOutput(f)
+
+	log.Println("--- Starting Mock HTTP Server ---")
+
+	// Load environment variables from .env file
+	err = godotenv.Load()
+	CheckErr(err)
+
+	SetupServerHandlers()
+	StartServer()
+}
+
+func CheckErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func SetupServerHandlers() {
+	http.HandleFunc("/echo", echoHandler)
+	http.HandleFunc("/echo/details", echoDetailsHandler)
+}
+
+func StartServer() {
+	// Get the server port from environment variables
+	port := os.Getenv("MOCK_SERVER_PORT")
+	if port == "" {
+		port = "8080" // Default to port 8080
+	}
+
+	// Start the HTTP server
+	log.Printf("Starting server on :%s\n", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Println("Failed to start server:", err)
+	}
+}
+
+func echoHandler(w http.ResponseWriter, r *http.Request) {
+	// Read the request body
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Failed to read request body", http.StatusInternalServerError)
+		return
+	}
+
+	// Echo the request body
+	w.Write(body)
+}
+
+func echoDetailsHandler(w http.ResponseWriter, r *http.Request) {
+	// Define a struct to hold the request details
+	type RequestDetails struct {
+		Method     string                 `json:"method"`
+		URL        string                 `json:"url"`
+		Proto      string                 `json:"proto"`
+		Host       string                 `json:"host"`
+		RemoteAddr string                 `json:"remote_addr"`
+		RequestURI string                 `json:"request_uri"`
+		UserAgent  string                 `json:"user_agent"`
+		Referer    string                 `json:"referer"`
+		Header     map[string][]string    `json:"header"`
+		BodyText   string                 `json:"bodyText"`
+		BodyJson   map[string]interface{} `json:"bodyJson"`
+	}
+
+	bodyBytes, err := io.ReadAll(r.Body)
+	CheckErr(err)
+
+	textBody := string(bodyBytes)
+
+	jsonBody := make(map[string]interface{})
+	if r.Header.Get("Content-Type") == "application/json" {
+		if err := json.Unmarshal(bodyBytes, &jsonBody); err != nil {
+			log.Printf("Failed to decode JSON body: %v", err)
+			jsonBody["error"] = "Failed to decode JSON body"
+			jsonBody["details"] = err.Error()
+		}
+	}
+
+	// Create an instance of the RequestDetails struct
+	details := RequestDetails{
+		Method:     r.Method,
+		URL:        r.URL.String(),
+		Proto:      r.Proto,
+		Host:       r.Host,
+		RemoteAddr: r.RemoteAddr,
+		RequestURI: r.RequestURI,
+		UserAgent:  r.UserAgent(),
+		Referer:    r.Referer(),
+		Header:     r.Header,
+		BodyText:   textBody,
+		BodyJson:   jsonBody,
+	}
+
+	// Convert the struct to JSON
+	jsonData, err := json.Marshal(details)
+	if err != nil {
+		http.Error(w, "Failed to convert to JSON", http.StatusInternalServerError)
+		return
+	}
+
+	// Set the response content type to JSON
+	w.Header().Set("Content-Type", "application/json")
+
+	// Write the JSON response
+	w.Write(jsonData)
+}
