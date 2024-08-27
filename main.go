91aa77166aa5ff250001413af241f2ed8b4a2fa9
Aidan Timson <aidan@timmo.dev>
2024-08-27 17:33:00 +0100

    Split main and server

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"io"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// Setup a logger to a file
+	f, err := os.OpenFile("mock-http-server-go.log", os.O_RDWR|os.O_CREATE|os.O_APPEND|os.O_TRUNC, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	log.SetOutput(io.MultiWriter(os.Stdout, f))
+
+	log.Println("--- Starting Mock HTTP Server ---")
+
+	// Load environment variables from .env file
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	SetupServerHandlers()
+	StartServer()
+}
--- server.go
+++ server.go
@@ -8,34 +8,11 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
-
-	"github.com/joho/godotenv"
 )
 
 type ResponseError struct {
 	Error   string `json:"error"`
 	Details string `json:"details"`
-}
-
-func main() {
-	// Setup a logger to a file
-	f, err := os.OpenFile("mock-http-server-go.log", os.O_RDWR|os.O_CREATE|os.O_APPEND|os.O_TRUNC, 0666)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
-	log.SetOutput(io.MultiWriter(os.Stdout, f))
-
-	log.Println("--- Starting Mock HTTP Server ---")
-
-	// Load environment variables from .env file
-	err = godotenv.Load()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	SetupServerHandlers()
-	StartServer()
 }
 
 func SetupServerHandlers() {
